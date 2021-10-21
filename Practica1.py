ce5e01f62b2d4aece92cb3becd4485d36d4364f6
Juan08302001 <92490482+Juan08302001@users.noreply.github.com>
2021-10-20 21:14:55 -0500

    Add files via upload

---
+++ Practica1.py
@@ -0,0 +1,8 @@
+a=input(float("Ingrese el numero que desea comprarar"))
+
+if a == 50:
+ print("El valor es igual a 50")
+elif a > 50:
+    print("Elvalor es mayor a 50")
+else
+        print("El valor es menor a 50")
